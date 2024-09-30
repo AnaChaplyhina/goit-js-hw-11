4b462fee50718af311aab39963db994fd6104e9b
Anastasiia Chaplyhina <chaplyhina.anastasiia@gmail.com>
2024-09-30 20:33:44 +0200

    upd

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,13 +1,6 @@
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '11281296-1f4fc3f6c48856d743f19a161'; 
 
-async function init() {
-  console.log(url);
-  const response = await fetch(url);
-}
-
-init(); 
-
 
 export async function fetchImages(query, page = 1) {
   const url = `${BASE_URL}?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&per_page=12&page=${page}`;
